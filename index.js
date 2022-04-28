741ca86701cecb6d23888d46a34b2a3347bd4866
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-04-27 21:22:56 -0400

    changes to Web Game Part IV to meet activity criteria

--- index.js
+++ index.js
@@ -1,15 +1,101 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
 
+// 5) Responding to a Character's Direction Change - creating a function to modify the character's image when it changes direction.
+function handleDirectionChange(direction){  //direction as a parameter so it's in scope
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+    
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+// This was 2) Write a moveCharacter Function of the activity. I've commented it out for reference later
+// let direction = null;
+// let x = 100;
+// let y = 250;
+// move(character).to(100, 250);
+
+// function moveCharacter() {
+//   if (direction === "west") {
+//     x = x - 1;
+//   }
+//   if (direction === "north") {
+//     y = y + 1;
+//   }
+//   if (direction === "east") {
+//     x = x + 1;
+//   }
+//   if (direction === "south") {
+//     y = y - 1;
+//   }
+//   character.style.left = x + "px";
+//   character.style.bottom = y + "px";
+// }
+
+// setInterval(moveCharacter, 1);
+
+// Defining function inline as sugggested in activity in module 3 web game part IV
+// setInterval(function(){
+//     if(direction === 'west'){
+//         x = x - 1
+//     }
+//     if(direction === 'north'){
+//         y = y + 1
+//     }
+//     if(direction === 'east'){
+//         x = x + 1
+//     }
+//     if(direction === 'south'){
+//         y = y - 1
+//     }
+//     character.style.left = x + 'px'
+//     character.style.bottom = y + 'px'
+// }, 1)
+
+// 3) Change the Character's Direction with Arrow Keys
+// document.addEventListener("keydown", function (e) {
+//   if (e.repeat) return;
+
+//   if (e.key === "ArrowLeft") {
+//     direction = "west";
+//   }
+//   if (e.key === "ArrowUp") {
+//     direction = "north";
+//   }
+//   if (e.key === "ArrowRight") {
+//     direction = "east";
+//   }
+//   if (e.key === "ArrowDown") {
+//     direction = "south";
+//   }
+// });
+
+// document.addEventListener("keyup", function (e) {
+//   direction = null;
+// });
+
+// move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
--- move.js
+++ move.js
@@ -1,12 +1,68 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
+
+// 4) Refactoring Our Code in activity about callback functions Web Game Part IV
+// 5) Responding to a Character's Direction Change - we need to call the callback where we change direction and we have to pass direction as a parameter in callback to have direction in scope.
+  function moveWithArrowKeys(left, bottom, callback) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+    
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+
+    function moveCharacter() {
+      if (direction === "west") {
+        x -= 1;
+      }
+      if (direction === "north") {
+        y += 1;
+      }
+      if (direction === "east") {
+        x += 1;
+      }
+      if (direction === "south") {
+        y -= 1;
+      }
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
     }
 
-    return {
-        to: moveToCoordinates
-    }
+    setInterval(moveCharacter, 1)
+
+    document.addEventListener("keydown", function (e) {
+        if (e.repeat) return;
+      
+        if (e.key === "ArrowLeft") {
+          direction = "west";
+        }
+        if (e.key === "ArrowUp") {
+          direction = "north";
+        }
+        if (e.key === "ArrowRight") {
+          direction = "east";
+        }
+        if (e.key === "ArrowDown") {
+          direction = "south";
+        }
+        callback(direction)  //direction as a parameter so it's in scope
+      });
+      
+      document.addEventListener("keyup", function (e) {
+        direction = null;
+        callback(direction)  //direction as a parameter so it's in scope
+      })
+  }
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
+
+
